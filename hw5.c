0430453514ae408ab1eb7a3058fa3e7b7f0b1248
foxihyun <tlgus0514@naver.com>
2022-10-13 15:02:21 +0900

    Create hw5.c
    .

---
+++ hw5.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int Convert(int num)
+{
+	if (num > 1) 
+		Convert(num / 2);
+	printf("%d", num % 2);
+}
+
+int main() 
+{
+	int num;
+	printf("Please enter a number: ");
+	scanf("%d", &num);
+	Convert(num);
+
+	return 0;
+}
